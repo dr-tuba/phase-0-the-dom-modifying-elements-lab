a88dffdaed7eecdb0cc54439894675ebbbee800a
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-04-22 18:04:01 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const body = document.getElementsByTagName('body')[0];
+body.removeChild(body.querySelector('main'));
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = '<h1>YOUR-NAME is the champion</h1>';
